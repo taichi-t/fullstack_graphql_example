cedb437b1e6e6e405c00c51519d8a77af55697cd
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-02-20 18:51:30 -0800

    :tada: Initial commit.

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+import { ApolloServer } from 'apollo-server';
+import resolvers from './resolvers';
+import typeDefs from './schema';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
